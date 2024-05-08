ca08f1d558814db62a4fe4a49952f9ca1614850f
Manuel Alonso <manuel.alonso.g@gmail.com>
2024-05-08 10:23:37 -0400

    Asunto: Solución del día 36.
    Descripción:
    
    * Se agregó la solución para el desafío del día 36.
    
    **Firma:**
    
    M. Alonso

---
+++ 36/count_string.py
@@ -0,0 +1,20 @@
+"""
+Day 36: Count String
+Write a function called count that takes one argument a string, and returns a dictionary of how many times
+each element appears in the string. For example ‘hello’ should return:
+{‘h’:1,’e’: 1,’l’:2, ‘o’:1}.
+"""
+
+
+def count(a: str):
+    count_dict = {}
+    for i in a:
+        if i in count_dict:
+            count_dict[i] += 1
+        else:
+            count_dict[i] = 1
+    return count_dict
+
+
+test_string = "hello"
+print(count(test_string))
